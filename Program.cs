db92a27ab5bdbf2a9ef30319fa230de5766e0f76
Lukasz Grad <lrg121@student.aru.ac.uk>
2022-11-03 15:23:06 +0000

    Task1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
-﻿int[] quantity = new int[] {6,8,12,5,10,17};
-
-for (int i=0; i<quantity.Length; i++)
+﻿//array of floats
+double[] array = new double[] {3.4,6.7,8.3,9.0,2.4};
+//for loop
+for (int i=0; i<array.Length; i++)
 {
-    Console.WriteLine("Quantity "+i+" = "+quantity[i]);
+    Console.WriteLine("Array "+i+" = "+array[i]);
 }
 
-foreach (int qty in quantity)
-{
-    Console.WriteLine("Quantity = "+qty);
-}
+
 
 Console.ReadKey();
