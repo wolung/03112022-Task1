3f4448c66a995d89c1377c9e0088e557c1427da9
Lukasz Grad <lrg121@student.aru.ac.uk>
2022-11-03 15:08:16 +0000

    Task1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int[] quantity = new int[] {6,8,12,5,10,17};
+
+for (int i=0; i<quantity.Length; i++)
+{
+    Console.WriteLine("Quantity "+i+" = "+quantity[i]);
+}
+
+foreach (int qty in quantity)
+{
+    Console.WriteLine("Quantity = "+qty);
+}
+
+Console.ReadKey();
